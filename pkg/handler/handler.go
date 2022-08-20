6779eb28a0625576836ba33befb7037d3b2ebbdc
Tobias Gurtzick <magic@wizardtales.com>
2022-08-20 16:42:45 +0200

    cluster handler
    Signed-off-by: Tobias Gurtzick <magic@wizardtales.com>

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -65,6 +65,7 @@ func (a *PodPresetMutator) Handle(ctx context.Context, req admission.Request) ad
 	}
 
 	podPresetList := &wzrdtalesscpv1alpha1.PodPresetList{}
+	clusterPodPresetList := &wzrdtalesscpv1alpha1.ClusterPodPresetList{}
 
 	err = a.Client.List(context.TODO(), podPresetList, &client.ListOptions{Namespace: req.Namespace})
 
@@ -72,18 +73,30 @@ func (a *PodPresetMutator) Handle(ctx context.Context, req admission.Request) ad
 		return admission.Errored(http.StatusInternalServerError, fmt.Errorf("Error retrieving ist of PodPresets: %v", err))
 	}
 
+	err = a.Client.List(context.TODO(), clusterPodPresetList, &client.ListOptions{})
+
+	if err != nil {
+		return admission.Errored(http.StatusInternalServerError, fmt.Errorf("Error retrieving ist of ClusterPodPresets: %v", err))
+	}
+
 	matchingPPs, err := filterPodPresets(*podPresetList, pod)
+	matchingCPPs, err := filterClusterPodPresets(*clusterPodPresetList, pod)
 	if err != nil {
 		return admission.Errored(http.StatusInternalServerError, fmt.Errorf("filtering pod presets failed: %v", err))
 	}
 
-	if len(matchingPPs) == 0 {
+	if len(matchingPPs) == 0 && len(matchingCPPs) == 0 {
 		return admission.Allowed("")
 	}
 
 	presetNames := make([]string, len(matchingPPs))
 	for i, pp := range matchingPPs {
 		presetNames[i] = pp.GetName()
+	}
+
+	clusterPresetNames := make([]string, len(matchingCPPs))
+	for i, pp := range matchingCPPs {
+		clusterPresetNames[i] = pp.GetName()
 	}
 
 	// detect merge conflict
@@ -95,7 +108,17 @@ func (a *PodPresetMutator) Handle(ctx context.Context, req admission.Request) ad
 		admission.Allowed("")
 	}
 
+	// detect merge conflict
+	err = safeToApplyClusterPodPresetsOnPod(pod, matchingCPPs)
+	if err != nil {
+		// conflict, ignore the error, but raise an event
+		logger.Info("conflict occurred while applying clusterpodpresets: %s on pod: %v err: %v",
+			strings.Join(presetNames, ","), pod.GetGenerateName(), err)
+		admission.Allowed("")
+	}
+
 	applyPodPresetsOnPod(pod, matchingPPs)
+	applyClusterPodPresetsOnPod(pod, matchingCPPs)
 
 	// End Mutation
 	marshaledPod, err := json.Marshal(pod)
@@ -134,6 +157,25 @@ func filterPodPresets(list wzrdtalesscpv1alpha1.PodPresetList, pod *corev1.Pod) 
 	return matchingPPs, nil
 }
 
+// filterPodPresets returns list of PodPresets which match given Pod.
+func filterClusterPodPresets(list wzrdtalesscpv1alpha1.ClusterPodPresetList, pod *corev1.Pod) ([]*wzrdtalesscpv1alpha1.ClusterPodPreset, error) {
+	var matchingPPs []*wzrdtalesscpv1alpha1.ClusterPodPreset
+
+	for _, pp := range list.Items {
+		selector, err := metav1.LabelSelectorAsSelector(&pp.Spec.Selector)
+		if err != nil {
+			return nil, fmt.Errorf("label selector conversion failed: %v for selector: %v", pp.Spec.Selector, err)
+		}
+
+		// check if the pod labels match the selector
+		if !selector.Matches(labels.Set(pod.Labels)) {
+			continue
+		}
+		matchingPPs = append(matchingPPs, &pp)
+	}
+	return matchingPPs, nil
+}
+
 // safeToApplyPodPresetsOnPod determines if there is any conflict in information
 // injected by given PodPresets in the Pod.
 func safeToApplyPodPresetsOnPod(pod *corev1.Pod, podPresets []*wzrdtalesscpv1alpha1.PodPreset) error {
@@ -142,6 +184,20 @@ func safeToApplyPodPresetsOnPod(pod *corev1.Pod, podPresets []*wzrdtalesscpv1alp
 	// volumes attribute is defined at the Pod level, so determine if volumes
 	// injection is causing any conflict.
 	if _, err := mergeVolumes(pod.Spec.Volumes, podPresets); err != nil {
+		errs = append(errs, err)
+	}
+
+	return utilerrors.NewAggregate(errs)
+}
+
+// safeToApplyPodPresetsOnPod determines if there is any conflict in information
+// injected by given PodPresets in the Pod.
+func safeToApplyClusterPodPresetsOnPod(pod *corev1.Pod, podPresets []*wzrdtalesscpv1alpha1.ClusterPodPreset) error {
+	var errs []error
+
+	// volumes attribute is defined at the Pod level, so determine if volumes
+	// injection is causing any conflict.
+	if _, err := mergeClusterVolumes(pod.Spec.Volumes, podPresets); err != nil {
 		errs = append(errs, err)
 	}
 
@@ -162,6 +218,45 @@ func safeToApplyPodPresetsOnContainer(ctr *corev1.Container, podPresets []*wzrdt
 	}
 
 	return utilerrors.NewAggregate(errs)
+}
+
+// mergeClusterEnv merges a list of env vars with the env vars injected by given list podPresets.
+// It returns an error if it detects any conflict during the merge.
+func mergeClusterEnv(envVars []corev1.EnvVar, podPresets []*wzrdtalesscpv1alpha1.ClusterPodPreset) ([]corev1.EnvVar, error) {
+	origEnv := map[string]corev1.EnvVar{}
+	for _, v := range envVars {
+		origEnv[v.Name] = v
+	}
+
+	mergedEnv := make([]corev1.EnvVar, len(envVars))
+	copy(mergedEnv, envVars)
+
+	var errs []error
+
+	for _, pp := range podPresets {
+		for _, v := range pp.Spec.Env {
+
+			found, ok := origEnv[v.Name]
+			if !ok {
+				// if we don't already have it append it and continue
+				origEnv[v.Name] = v
+				mergedEnv = append(mergedEnv, v)
+				continue
+			}
+
+			// make sure they are identical or throw an error
+			if !reflect.DeepEqual(found, v) {
+				errs = append(errs, fmt.Errorf("merging env for %s has a conflict on %s: \n%#v\ndoes not match\n%#v\n in container", pp.GetName(), v.Name, v, found))
+			}
+		}
+	}
+
+	err := utilerrors.NewAggregate(errs)
+	if err != nil {
+		return nil, err
+	}
+
+	return mergedEnv, err
 }
 
 // mergeEnv merges a list of env vars with the env vars injected by given list podPresets.
@@ -220,6 +315,39 @@ func newEnvFromMergeKey(e corev1.EnvFromSource) envFromMergeKey {
 	return k
 }
 
+func mergeClusterEnvFrom(envSources []corev1.EnvFromSource, podPresets []*wzrdtalesscpv1alpha1.ClusterPodPreset) ([]corev1.EnvFromSource, error) {
+	var mergedEnvFrom []corev1.EnvFromSource
+
+	// merge envFrom using a identify key to ensure Admit reinvocations are idempotent
+	origEnvSources := map[envFromMergeKey]corev1.EnvFromSource{}
+	for _, envSource := range envSources {
+		origEnvSources[newEnvFromMergeKey(envSource)] = envSource
+	}
+	mergedEnvFrom = append(mergedEnvFrom, envSources...)
+	var errs []error
+	for _, pp := range podPresets {
+		for _, envFromSource := range pp.Spec.EnvFrom {
+
+			found, ok := origEnvSources[newEnvFromMergeKey(envFromSource)]
+			if !ok {
+				mergedEnvFrom = append(mergedEnvFrom, envFromSource)
+				continue
+			}
+			if !reflect.DeepEqual(found, envFromSource) {
+				errs = append(errs, fmt.Errorf("merging envFrom for %s has a conflict: \n%#v\ndoes not match\n%#v\n in container", pp.GetName(), envFromSource, found))
+			}
+		}
+
+	}
+
+	err := utilerrors.NewAggregate(errs)
+	if err != nil {
+		return nil, err
+	}
+
+	return mergedEnvFrom, nil
+}
+
 func mergeEnvFrom(envSources []corev1.EnvFromSource, podPresets []*wzrdtalesscpv1alpha1.PodPreset) ([]corev1.EnvFromSource, error) {
 	var mergedEnvFrom []corev1.EnvFromSource
 
@@ -251,6 +379,59 @@ func mergeEnvFrom(envSources []corev1.EnvFromSource, podPresets []*wzrdtalesscpv
 	}
 
 	return mergedEnvFrom, nil
+}
+
+// mergeClusterVolumeMounts merges given list of VolumeMounts with the volumeMounts
+// injected by given podPresets. It returns an error if it detects any conflict during the merge.
+func mergeClusterVolumeMounts(volumeMounts []corev1.VolumeMount, podPresets []*wzrdtalesscpv1alpha1.ClusterPodPreset) ([]corev1.VolumeMount, error) {
+
+	origVolumeMounts := map[string]corev1.VolumeMount{}
+	volumeMountsByPath := map[string]corev1.VolumeMount{}
+	for _, v := range volumeMounts {
+		origVolumeMounts[v.Name] = v
+		volumeMountsByPath[v.MountPath] = v
+	}
+
+	mergedVolumeMounts := make([]corev1.VolumeMount, len(volumeMounts))
+	copy(mergedVolumeMounts, volumeMounts)
+
+	var errs []error
+
+	for _, pp := range podPresets {
+		for _, v := range pp.Spec.VolumeMounts {
+
+			found, ok := origVolumeMounts[v.Name]
+			if !ok {
+				// if we don't already have it append it and continue
+				origVolumeMounts[v.Name] = v
+				mergedVolumeMounts = append(mergedVolumeMounts, v)
+			} else {
+				// make sure they are identical or throw an error
+				// shall we throw an error for identical volumeMounts ?
+				if !reflect.DeepEqual(found, v) {
+					errs = append(errs, fmt.Errorf("merging volume mounts for %s has a conflict on %s: \n%#v\ndoes not match\n%#v\n in container", pp.GetName(), v.Name, v, found))
+				}
+			}
+
+			found, ok = volumeMountsByPath[v.MountPath]
+			if !ok {
+				// if we don't already have it append it and continue
+				volumeMountsByPath[v.MountPath] = v
+			} else {
+				// make sure they are identical or throw an error
+				if !reflect.DeepEqual(found, v) {
+					errs = append(errs, fmt.Errorf("merging volume mounts for %s has a conflict on mount path %s: \n%#v\ndoes not match\n%#v\n in container", pp.GetName(), v.MountPath, v, found))
+				}
+			}
+		}
+	}
+
+	err := utilerrors.NewAggregate(errs)
+	if err != nil {
+		return nil, err
+	}
+
+	return mergedVolumeMounts, err
 }
 
 // mergeVolumeMounts merges given list of VolumeMounts with the volumeMounts
@@ -349,6 +530,79 @@ func mergeVolumes(volumes []corev1.Volume, podPresets []*wzrdtalesscpv1alpha1.Po
 	return mergedVolumes, err
 }
 
+// mergeClusterVolumes merges given list of Volumes with the volumes injected by given
+// podPresets. It returns an error if it detects any conflict during the merge.
+func mergeClusterVolumes(volumes []corev1.Volume, podPresets []*wzrdtalesscpv1alpha1.ClusterPodPreset) ([]corev1.Volume, error) {
+	origVolumes := map[string]corev1.Volume{}
+	for _, v := range volumes {
+		origVolumes[v.Name] = v
+	}
+
+	mergedVolumes := make([]corev1.Volume, len(volumes))
+	copy(mergedVolumes, volumes)
+
+	var errs []error
+
+	for _, pp := range podPresets {
+		for _, v := range pp.Spec.Volumes {
+
+			found, ok := origVolumes[v.Name]
+			if !ok {
+				// if we don't already have it append it and continue
+				origVolumes[v.Name] = v
+				mergedVolumes = append(mergedVolumes, v)
+				continue
+			}
+
+			// make sure they are identical or throw an error
+			if !reflect.DeepEqual(found, v) {
+				errs = append(errs, fmt.Errorf("merging volumes for %s has a conflict on %s: \n%#v\ndoes not match\n%#v\n in container", pp.GetName(), v.Name, v, found))
+			}
+		}
+	}
+
+	err := utilerrors.NewAggregate(errs)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(mergedVolumes) == 0 {
+		return nil, nil
+	}
+
+	return mergedVolumes, err
+}
+
+// applyClusterPodPresetsOnPod updates the PodSpec with merged information from all the
+// applicable PodPresets. It ignores the errors of merge functions because merge
+// errors have already been checked in safeToApplyPodPresetsOnPod function.
+func applyClusterPodPresetsOnPod(pod *corev1.Pod, podPresets []*wzrdtalesscpv1alpha1.ClusterPodPreset) {
+	if len(podPresets) == 0 {
+		return
+	}
+
+	volumes, _ := mergeClusterVolumes(pod.Spec.Volumes, podPresets)
+	pod.Spec.Volumes = volumes
+
+	for i, ctr := range pod.Spec.Containers {
+		applyClusterPodPresetsOnContainer(&ctr, podPresets)
+		pod.Spec.Containers[i] = ctr
+	}
+	for i, iCtr := range pod.Spec.InitContainers {
+		applyClusterPodPresetsOnContainer(&iCtr, podPresets)
+		pod.Spec.InitContainers[i] = iCtr
+	}
+
+	// add annotation
+	if pod.ObjectMeta.Annotations == nil {
+		pod.ObjectMeta.Annotations = map[string]string{}
+	}
+
+	for _, pp := range podPresets {
+		pod.ObjectMeta.Annotations[fmt.Sprintf("%s/podpreset-%s", annotationPrefix, pp.GetName())] = pp.GetResourceVersion()
+	}
+}
+
 // applyPodPresetsOnPod updates the PodSpec with merged information from all the
 // applicable PodPresets. It ignores the errors of merge functions because merge
 // errors have already been checked in safeToApplyPodPresetsOnPod function.
@@ -377,6 +631,20 @@ func applyPodPresetsOnPod(pod *corev1.Pod, podPresets []*wzrdtalesscpv1alpha1.Po
 	for _, pp := range podPresets {
 		pod.ObjectMeta.Annotations[fmt.Sprintf("%s/podpreset-%s", annotationPrefix, pp.GetName())] = pp.GetResourceVersion()
 	}
+}
+
+// applyClusterPodPresetsOnContainer injects envVars, VolumeMounts and envFrom from
+// given podPresets in to the given container. It ignores conflict errors
+// because it assumes those have been checked already by the caller.
+func applyClusterPodPresetsOnContainer(ctr *corev1.Container, podPresets []*wzrdtalesscpv1alpha1.ClusterPodPreset) {
+	envVars, _ := mergeClusterEnv(ctr.Env, podPresets)
+	ctr.Env = envVars
+
+	volumeMounts, _ := mergeClusterVolumeMounts(ctr.VolumeMounts, podPresets)
+	ctr.VolumeMounts = volumeMounts
+
+	envFrom, _ := mergeClusterEnvFrom(ctr.EnvFrom, podPresets)
+	ctr.EnvFrom = envFrom
 }
 
 // applyPodPresetsOnContainer injects envVars, VolumeMounts and envFrom from
